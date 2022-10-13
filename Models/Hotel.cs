4f70995c819fa18a73341f2d6d8be3ef15da6bc7
kawjaw66 <kawjaw66@gmail.com>
2022-10-13 16:11:19 +0700

    *Hotel model

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -4,14 +4,20 @@ using System.Text.Json.Serialization;
 
 namespace GypooWebAPI.Models
 {
-    public class Hotel{
+    public class Hotel
+    {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id {get;set;}
+        public string? Id { get; set; }
 
-        public string Name {get; set;} = null!;
+        public string Name { get; set; } = null!;
 
+        public float Rating { get; set; } = 0;
+
+        public string Location { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
         
-        public List<string> roomIds {get; set;} = null!;
+        public List<string> roomIds { get; set; } = null!;
     }
 }
