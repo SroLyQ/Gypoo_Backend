d4c80260e6144dbd61eadf0425ca6b843c89d36e
Makasan <63010791@KMITL.AC.TH>
2022-10-31 22:37:52 +0700

    AddServiceRoom

--- Models/Room.cs
+++ Models/Room.cs
@@ -7,14 +7,24 @@ namespace GypooWebAPI.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? idRoom { get; set; } = null!;
-        public string sideBed { get; set; } = null!;
+
+        // public string sideBed { get; set; } = null!;
 
         public float price { get; set; } = 0;
 
         public List<string> facilities { get; set; } = null!;
 
-        public List<string> service { get; set; } = null!;
+        public List<ServiceRoom> service { get; set; } = null!;
 
         public string comment { get; set; } = null!;
     }
+
+    public class ServiceRoom
+    {
+
+        // public string sideBed { get; set; } = null!;
+        public bool breakFast { get; set; } = false!;
+
+    }
+
 }
