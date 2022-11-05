cc7f7491f7822adcbb010c043af72ad52587f440
kawjaw66 <kawjaw66@gmail.com>
2022-11-05 23:26:49 +0700

    *hotel model

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -10,15 +10,24 @@ namespace GypooWebAPI.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
+        public int index { get; set; } = 0;
+        public bool isAvailable { get; set; } = true;
+        public List<string> picture { get; set; } = null!;
+        public string name { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public string phone { get; set; } = null!;
+        public string address { get; set; } = null!;
+        public string about { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+        public string latitude { get; set; } = null!;
+        public string longtitude { get; set; } = null!;
+        public float discount { get; set; } = 0;
+        public List<string> facilities { get; set; } = null!;
 
-        public float Rating { get; set; } = 0;
+        public float rating { get; set; } = 0;
 
-        public string Location { get; set; } = null!;
-
-        public string Description { get; set; } = null!;
-
+        public int review { get; set; } = 0;
+        public List<string> comment { get; set; } = null!;
         public List<string> roomIds { get; set; } = null!;
     }
 }
