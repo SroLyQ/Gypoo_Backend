632b55607256a437b17d25f21759aeda03ab9621
kawjaw66 <kawjaw66@gmail.com>
2022-11-20 03:23:31 +0700

    +Promotion

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSingleton<UserService>();
 builder.Services.AddSingleton<RoomService>();
 builder.Services.AddSingleton<UploadService>();
 builder.Services.AddSingleton<CommentService>();
+builder.Services.AddSingleton<PromotionService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
