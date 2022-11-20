d4a30ea8f03d05f6a1909919e08ecc2d9132bec7
Makasan <63010791@KMITL.AC.TH>
2022-11-21 01:22:09 +0700

    addDiscountInRoom

--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -9,10 +9,12 @@ namespace GypooWebAPI.Services
     {
         private MongoDBService _mongoDBSevice;
         private IMongoCollection<Room> _roomCollection;
+        private IMongoCollection<Hotel> _hotelCollection;
         public RoomService(MongoDBService mongoDBService)
         {
             _mongoDBSevice = mongoDBService;
             _roomCollection = _mongoDBSevice._roomCollection;
+            _hotelCollection = mongoDBService._hotelCollection;
         }
 
         public async Task CreateRoomAsync(Room room)
@@ -27,11 +29,43 @@ namespace GypooWebAPI.Services
                 room.roomCount30Day.Add(nowAva);
             }
             await _roomCollection.InsertOneAsync(room);
+
+            // Room _room = room;
+            // List<Room> rooms = await this.GetRoomByHotelId(room.idHotel);
+            // double rating = this.rateCalculation(comments);
+            // UpdateDefinition<Hotel> update = Builders<Hotel>.Update.Set<double>("rating", rating);
+            // await _roomCollection.UpdateOneAsync(_hotel => _hotel.Id == room.idHotel, update);
             return;
+        }
+        // public async Task<List<Room>> GetRoomByHotelId(string hotelId)
+        // {
+        //     List<Room> Rooms = await _roomCollection.Find(_Room => _Room.idHotel == hotelId).ToListAsync();
+        //     return Rooms;
+        // }
+
+        // public async Task<List<Room>> GetRoomsAva()
+        // {
+
+        //     return await _roomCollection.Find(new BsonDocument()).ToListAsync();
+        // }
+        private async Task<List<Room>> updateDiscount()
+        {
+            // Room room = await _roomCollection.Find(_room => _room.idRoom == id).SingleAsync();
+            // Hotel hotel = await _hotelCollection.Find(_hotel => _hotel.Id == id).SingleAsync();
+            List<Room> rooms = await _roomCollection.Find(new BsonDocument()).ToListAsync();
+            List<Hotel> hotels = await _hotelCollection.Find(new BsonDocument()).ToListAsync();
+            foreach (var hotel in hotels)
+            {
+                var update = Builders<Room>.Update.Set("discount", hotel.discount);
+                Room newRoom = await _roomCollection.FindOneAndUpdateAsync(_room => _room.idRoom == _room.idRoom, update);
+            }
+            return rooms;
         }
 
         public async Task<List<Room>> GetRoomsAsync()
         {
+            // List<Room> rooms = await updateDiscount();
+            // return rooms;
             return await _roomCollection.Find(new BsonDocument()).ToListAsync();
         }
 
